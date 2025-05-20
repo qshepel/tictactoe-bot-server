4258fdaa8ff1ce532a75532953ad74cca4ecfc6e
qshepel <shepel.semen.k@gmail.com>
2025-05-21 01:30:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ import requests
 app = Flask(__name__)
 
 BOT_TOKEN = "7794390382:AAEtr0y3a-PArzp5bKTjm4eJqQFfneMDWeo"
-WEB_APP_URL = "https://tictactoe-miniapp.vercel.app"  # временный адрес
+WEB_APP_URL = "https://tictactoe-miniapp.vercel.app"  # ссылка на мини-приложение
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot is running", 200
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    if "message" in data and "text" in data["message"]:
+    if data and "message" in data and "text" in data["message"]:
         chat_id = data["message"]["chat"]["id"]
         if data["message"]["text"].lower() == "/start":
             send_webapp_button(chat_id)
-    return "ok"
+    return "ok", 200
 
 def send_webapp_button(chat_id):
     requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", json={
@@ -28,5 +32,4 @@ def send_webapp_button(chat_id):
     })
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
